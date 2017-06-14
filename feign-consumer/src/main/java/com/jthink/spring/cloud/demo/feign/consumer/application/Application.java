a66ee312fd520b4534a5b845cecef3fbd0418aec
JThink <leviqian@sina.com>
2017-06-14 16:34:51 +0800

    使用starter

--- feign-consumer/src/main/java/com/jthink/spring/cloud/demo/feign/consumer/application/Application.java
+++ feign-consumer/src/main/java/com/jthink/spring/cloud/demo/feign/consumer/application/Application.java
@@ -1,16 +1,14 @@
 package com.jthink.spring.cloud.demo.feign.consumer.application;
 
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.web.client.RestTemplate;
 
 /**y
  * JThink@JThink
@@ -29,11 +27,7 @@ public class Application {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
 
-    @Bean
-    @LoadBalanced
-    RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
+    private static PoolingHttpClientConnectionManager cm = null;
 
     public static void main(String[] args) {
         new SpringApplicationBuilder(Application.class).run(args);
